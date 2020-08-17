50d7a051e2581a7105ec49edb8477d75bb6c0e70
ipeksargin <ipek.srgn@gmail.com>
2020-08-17 15:38:36 +0300

    test file added

---
+++ Test.php
@@ -0,0 +1,11 @@
+<?php
+
+use JotForm\JotForm;
+
+$jotForm = new JotForm("12345678");
+$jotForm->setBaseURL("mycompany.jotform.api");
+$jotForm->getFolder()->getFolder();
+$jotForm->folder->getFolder();
+
+$jotForm->getUser()->getUser();
+$jotForm->user->getUser();
