e3841ff3a69f02074e963d6b3845046ade3e9158
ipeksargin <ipek.srgn@gmail.com>
2020-09-01 19:46:05 +0300

    remove phar

--- src/Test.php
+++ src/Test.php
@@ -5,7 +5,7 @@ require dirname(__FILE__) . "/../vendor/autoload.php";
 use JotForm\JotForm;
 
 $jotForm = JotForm::create("d1ea30ef56105ff45c1e9125aaf724c1");
-//var_dump($jotForm->users->getUser());
+var_dump($jotForm->users->getUser());
 //var_dump($jotForm->forms->getForm("202162441844045"));
 //var_dump($jotForm->forms->cloneForm("202162441844045"));
 //print_r($jotForm->forms->deleteForm("202162441844045"));
