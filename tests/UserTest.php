3ec140f977a7a9bab9c090a32a0805371ce652c2
ipeksargin <ipek.srgn@gmail.com>
2020-09-03 16:24:48 +0300

    user test updated

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -5,7 +5,15 @@ use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Exception\RequestException;
+use JotForm\Errors\AuthorizationException;
+use JotForm\Errors\BadGatewayException;
 use JotForm\Errors\BadRequestException;
+use JotForm\Errors\DefaultException;
+use JotForm\Errors\ForbiddenException;
+use JotForm\Errors\NotFoundException;
+use JotForm\Errors\NotImplementedException;
+use JotForm\Errors\ServerException;
+use JotForm\Errors\ServiceUnavailableException;
 use JotForm\JotForm;
 use JotForm\RequestHandler;
 use PHPUnit\Framework\TestCase;
@@ -237,15 +245,132 @@ class UserTest extends TestCase
         );
     }
 
-    public function testGetUserShouldThrowException()
+    public function testGetUserShouldThrowAuthorizationException()
+    {
+        $this->expectException(AuthorizationException::class);
+
+        $mock = new MockHandler([
+            new Response(401),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $jotForm = new JotForm(new RequestHandler($client));
+        $jotForm->users->getUser();
+    }
+
+    public function testGetUserShouldThrowBadGatewayException()
+    {
+        $this->expectException(BadGatewayException::class);
+
+        $mock = new MockHandler([
+            new Response(502),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $jotForm = new JotForm(new RequestHandler($client));
+        $jotForm->users->getUser();
+    }
+
+    public function testGetUserShouldThrowBadRequestException()
     {
         $this->expectException(BadRequestException::class);
 
         $mock = new MockHandler([
-            new BadRequestException(
-                'Error Communicating with Server',
-                400
-            )
+            new Response(400),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $jotForm = new JotForm(new RequestHandler($client));
+        $jotForm->users->getUser();
+    }
+
+    public function testGetUserShouldThrowForbiddenException()
+    {
+        $this->expectException(ForbiddenException::class);
+
+        $mock = new MockHandler([
+            new Response(403),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $jotForm = new JotForm(new RequestHandler($client));
+        $jotForm->users->getUser();
+    }
+
+    public function testGetUserShouldThrowNotFoundException()
+    {
+        $this->expectException(NotFoundException::class);
+
+        $mock = new MockHandler([
+            new Response(404),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $jotForm = new JotForm(new RequestHandler($client));
+        $jotForm->users->getUser();
+    }
+
+    public function testGetUserShouldThrowNotImplementedException()
+    {
+        $this->expectException(NotImplementedException::class);
+
+        $mock = new MockHandler([
+            new Response(501),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $jotForm = new JotForm(new RequestHandler($client));
+        $jotForm->users->getUser();
+    }
+
+    public function testGetUserShouldThrowServerException()
+    {
+        $this->expectException(ServerException::class);
+
+        $mock = new MockHandler([
+            new Response(500),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $jotForm = new JotForm(new RequestHandler($client));
+        $jotForm->users->getUser();
+    }
+
+    public function testGetUserShouldThrowServiceUnavailableException()
+    {
+        $this->expectException(ServiceUnavailableException::class);
+
+        $mock = new MockHandler([
+            new Response(503),
+        ]);
+
+        $handlerStack = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handlerStack]);
+
+        $jotForm = new JotForm(new RequestHandler($client));
+        $jotForm->users->getUser();
+    }
+
+    public function testGetUserShouldThrowException()
+    {
+        $this->expectException(Exception::class);
+
+        $mock = new MockHandler([
+            new RequestException('Error Communicating with Server', new Request('GET', 'test'))
         ]);
 
         $handlerStack = HandlerStack::create($mock);
